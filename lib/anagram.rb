208ad6e183021f1b6258a27974f8c1f7860bd77f
Ryan Brady <ryanbrady369@gmail.com>
2018-10-30 20:38:44 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  @@matches = []
+
+  def initialize(word)
+    @word = word.split
+  end 
+
+  def match(list)
+    list.each do |curr|
+      if curr.length == @word.length
+        if @word.all? {|letter| curr.include?(letter)}
+          @@matches << curr
+        end         
+      end 
+    end 
+    @@matches
+  end 
+
+end 
+    
+    
